36bc19dc28c265a3c1b8b145b1f74cebb8c25c5a
AliexBabskiy <ababskiy@gmail.com>
2023-09-27 22:15:41 +0300

    V2f1

--- main.py
+++ main.py
@@ -17,61 +17,145 @@ while True:
     choice = input("Выберите действие: ")
     if choice == '1':
         print("Действие: сложение двух чисел \n")
-        num1 = int(input("Введите первое число: "))
-        num2 = int(input("Введите второе число: "))
+        while True:
+            try:
+                num1 = int(input("Введите первое число: "))
+                break
+            except ValueError:
+                print("Ошибка, введено некорректное значение ")
+        while True:
+            try:
+                num2 = int(input("Введите второе число: "))
+                break
+            except ValueError:
+                print("Ошибка, введено некорректное значение ")
         result = num1 + num2
     elif choice == '2':
         print("Действие: вычесть первое число из второго \n")
-        num1 = int(input("Введите первое число: "))
-        num2 = int(input("Введите второе число: "))
+        while True:
+            try:
+                num1 = int(input("Введите первое число: "))
+                break
+            except ValueError:
+                print("Ошибка, введено некорректное значение ")
+        while True:
+            try:
+                num2 = int(input("Введите второе число: "))
+                break
+            except ValueError:
+                print("Ошибка, введено некорректное значение ")
         result = num1 - num2
     elif choice == '3':
         print("Действие: перемножить два числа \n")
-        num1 = int(input("Введите первое число: "))
-        num2 = int(input("Введите второе число: "))
+        while True:
+            try:
+                num1 = int(input("Введите первое число: "))
+                break
+            except ValueError:
+                print("Ошибка, введено некорректное значение ")
+        while True:
+            try:
+                num2 = int(input("Введите второе число: "))
+                break
+            except ValueError:
+                print("Ошибка, введено некорректное значение ")
         result = num1 * num2
     elif choice == '4':
         print("Действие: Разделить первое на второе \n")
-        num1 = int(input("Введите первое число: "))
-        num2 = int(input("Введите второе число: "))
         while True:
-            num2 = int(input("Введите второе число: "))
-            if num2 == 0:
+            try:
+                num1 = int(input("Введите первое число: "))
+                break
+            except ValueError:
+                print("Ошибка, введено некорректное значение ")
+        exit = False
+        while exit == False:
+            while True:
+                try:
+                    num2 = int(input("Введите второе число: "))
+                    break
+                except ValueError:
+                    print("Ошибка, введено некорректное значение ")
+            if num2 != 0:
+                result = num1 / num2
+                exit = True
+            else:
                 print("Ошибка: делить на 0 нельзя.")
-                continue
-        result = num1 / num2
+        exit = False
     elif choice == '5':
         print("Действие: возвести в степень N первое число \n")
-        num1 = int(input("Введите первое число: "))
+        while True:
+            try:
+                num1 = int(input("Введите первое число: "))
+                break
+            except ValueError:
+                print("Ошибка, введено некорректное значение ")
         num2 = int(input("Введите степень: "))
+        while True:
+            try:
+                num2 = int(input("Введите степень: "))
+                break
+            except ValueError:
+                print("Ошибка, введено некорректное значение ")
         result = num1 ** num2
     elif choice == '6':
         print("Действие: найти квадратный корень из числа \n")
-        while True:
-            num1 = int(input("Введите число: "))
-            if num1 <= 0:
+        exit = False
+        while exit == False:
+            while True:
+                try:
+                    num1 = int(input("Введите число: "))
+                    break
+                except ValueError:
+                    print("Ошибка, введено некорректное значение ")
+            if num1 > 0:
+                result = math.sqrt(num1)
                 print("Ошибка: квадратный корень отрицательного числа.")
-                continue
-        result = math.sqrt(num1)
+            else:
+                print("Ошибка: квадратный корень отрицательного числа.")
+        exit = False
     elif choice == '7':
         print("Действие: найти факториал из числа \n")
-        while True:
-            num1 = int(input("Введите число: "))
-            if num1 < 0:
+        exit = False
+        while exit == False:
+            while True:
+                try:
+                    num1 = int(input("Введите число: "))
+                    break
+                except ValueError:
+                    print("Ошибка, введено некорректное значение ")
+            if num1 >= 0:
+                result = math.factorial(int(num1))
+                exit = True
+            else:
                 print("Ошибка: факториал отрицательного числа.")
-                continue
-        result = math.factorial(int(num1))
+        exit = False
     elif choice == '8':
         print("Действие: нахождение sin \n")
-        num1 = int(input("Введите число: "))
+        while True:
+            try:
+                num1 = int(input("Введите число: "))
+                break
+            except ValueError:
+                print("Ошибка, введено некорректное значение ")
         result = math.sin(num1)
     elif choice == '9':
         print("Действие: нахождение cos \n")
-        num1 = int(input("Введите число: "))
+        while True:
+            try:
+                num1 = int(input("Введите число: "))
+                break
+            except ValueError:
+                print("Ошибка, введено некорректное значение ")
         result = math.cos(num1)
     elif choice == '10':
         print("Действие: нахождение tg \n")
-        num1 = int(input("Введите число: "))
+        while True:
+            try:
+                num1 = int(input("Введите число: "))
+                break
+            except ValueError:
+                print("Ошибка, введено некорректное значение ")
         result = math.tan(num)
     elif choice == '0':
         print("Завершение работы.")
